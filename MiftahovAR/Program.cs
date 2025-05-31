a762f3cb9ff2a10bc3bb560eee51c1d1f8caba8b
AidarT001AT <aidar.miftakhof@yandex.ru>
2025-05-31 15:18:38 +0300

    delete solve task2

--- MiftahovAR/Program.cs
+++ MiftahovAR/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Мифтахов Айдар Расилович, Ам-1-24
-//solve task2
 static public double Sqr(double X)
